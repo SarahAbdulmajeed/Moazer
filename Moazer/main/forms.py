4fff7194121f06d2094ff93b0bc80b06ebb6ad5c
lujain415 <lujainalaamri55@gmail.com>
2025-08-23 22:30:31 +0300

    Add expert/student profile page and forms

--- Moazer/main/forms.py
+++ Moazer/main/forms.py
@@ -1,18 +1,23 @@
 from django import forms
-from .models import User, StudentProfile, ExpertProfile
+from django.contrib.auth import get_user_model
+from .models import StudentProfile, ExpertProfile
+
+User = get_user_model()
+
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'gender', 'dob',
-                  'phone', 'city', 'bio', 'profile_picture']
+        fields = ["first_name","last_name","gender","dob","phone","city","bio","profile_picture"]
+
 
 class StudentProfileForm(forms.ModelForm):
     class Meta:
         model = StudentProfile
-        fields = ['stage']
+        fields = ["stage"]
+
 
 class ExpertProfileForm(forms.ModelForm):
     class Meta:
         model = ExpertProfile
-        fields = ['iban', 'hourly_price']
+        fields = ["iban","hourly_price"]
