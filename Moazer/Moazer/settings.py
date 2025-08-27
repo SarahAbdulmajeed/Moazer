8a33db96dc9b398d749c76f9ca79d37cc7d30e25
lujain415 <lujainalaamri55@gmail.com>
2025-08-27 22:50:31 +0300

    email

--- Moazer/Moazer/settings.py
+++ Moazer/Moazer/settings.py
@@ -132,3 +132,11 @@ MEDIA_ROOT = BASE_DIR / "media"
 TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
 LOGIN_REDIRECT_URL = "/consultations/"     # بعد اللوقن يروح للقائمة
 LOGOUT_REDIRECT_URL = "/accounts/login/"   # بعد اللوغ آوت يرجع للوقن
+
+EMAIL_HOST_USER = 'moazer532@gmail.com'
+EMAIL_HOST_PASSWORD = 'dsar qdij fnmb rmzk'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
